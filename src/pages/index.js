95454ec6fd3cf01ecc03ab5c4a69afea1c54bb3e
Mohammed Aljefri <algfry12@gmail.com>
2019-09-15 20:00:19 +0300

    enhacements

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 import styled from "styled-components"
-import { ChevronDown } from "react-feather";
+import { ChevronDown } from "react-feather"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Wrapper from "../components/wrapper";
-import Input from "../components/input";
+import Wrapper from "../components/wrapper"
+import Input from "../components/input"
 
 const StyledForm = styled.form`
   width: 50%;
@@ -15,12 +15,9 @@ const StyledForm = styled.form`
 `
 
 const StyledCover = styled.div`
-<<<<<<< 890fcd4ea24ea04ed4f6fe41f0b5f3bb59e0757e
-  min-height: 100vh;
+  height: 100vh;
+  max-height: 700px;
   padding: 100px 0;
-=======
-  min-height: calc(100vh - 60px);
->>>>>>> enhancememtn
   position: relative;
   display: flex;
   justify-content: center;
@@ -41,7 +38,7 @@ const StyledHeader = styled.h1`
 
 const StyledSection = styled.section`
   display: flex;
-  flex-direction: ${({reverse}) => reverse && 'row-reverse'};
+  flex-direction: ${({ reverse }) => reverse && "row-reverse"};
 `
 
 const StyledInnerSection = styled.div`
@@ -65,13 +62,11 @@ const Section = ({ hint, title, paragraph, style, reverse }) => (
           <BigH2>{title}</BigH2>
           <p style={{ fontSize: 25 }}>{paragraph}</p>
         </StyledInnerSection>
-        <div>
-          {/* <Img src="te" /> */}
-        </div>
+        <div>{/* <Img src="te" /> */}</div>
       </StyledSection>
     </Wrapper>
   </section>
-);
+)
 
 const StyledSpan = styled.span`
   position: absolute;
@@ -86,7 +81,7 @@ const StyledSpan = styled.span`
   align-content: center;
   justify-content: center;
   ::before {
-    content: '';
+    content: "";
     position: absolute;
     top: 0;
     right: calc(50% - 25px);
@@ -101,7 +96,7 @@ const StyledSpan = styled.span`
   @keyframes pulsate {
     0% {
       transform: scale(1);
-      opacity: .8;
+      opacity: 0.8;
     }
     45% {
       transform: scale(1.75);
@@ -111,37 +106,40 @@ const StyledSpan = styled.span`
 `
 
 const KEYWORDS = [
-  'Yemenies',
-  'web developers',
-  'web',
-  'developers',
-  'mobile',
-  'ios',
-  'react',
-  'native',
-  'angular',
-  'vuejs',
-  'js',
-  'c#',
-  'c++',
-  'Ruby',
-];
+  "Yemen",
+  "web developers",
+  "web",
+  "developers",
+  "mobile",
+  "ios",
+  "react",
+  "native",
+  "angular",
+  "vuejs",
+  "js",
+  "c#",
+  "c++",
+  "Ruby",
+]
 
 const sections = [
   {
-    style: { background: '#c0170b', color: '#fff' },
-    title: 'Students',
-    paragraph: 'Our students come from a variety of backgrounds. Some want to become full-time developers, whereas some would like to learn the basics of coding in a supportive environment. \nSign up to join our vibrant community and attend our workshops.'
+    style: { background: "#c0170b", color: "#fff" },
+    title: "Students",
+    paragraph:
+      "Our students come from a variety of backgrounds. Some want to become full-time developers, whereas some would like to learn the basics of coding in a supportive environment. \nSign up to join our vibrant community and attend our workshops.",
   },
   {
-    title: 'Coaches',
+    title: "Coaches",
     reverse: true,
-    paragraph: 'We encourage new students to work on our HTML/CSS, JavaScript, Ruby, Python or Git tutorials. We also help them understand programming concepts.\n\nTherefore are always on the lookout for more developers to join our community and help coach at our workshops.'
+    paragraph:
+      "We encourage new students to work on our HTML/CSS, JavaScript, Ruby, Python or Git tutorials. We also help them understand programming concepts.\n\nTherefore are always on the lookout for more developers to join our community and help coach at our workshops.",
   },
   {
-    style: { background: '#101010', color: '#fff' },
-    title: 'Workshops',
-    paragraph: 'We are hosted by some amazing companies who support our cause. Our normal format is 30 minutes of socialising with food and drink, followed by a lightning talk then coding.\n\nHelp our expansion and enable us to provide help and support to our chapters by hosting and sponsoring our workshops.'
+    style: { background: "#101010", color: "#fff" },
+    title: "Workshops",
+    paragraph:
+      "We are hosted by some amazing companies who support our cause. Our normal format is 30 minutes of socialising with food and drink, followed by a lightning talk then coding.\n\nHelp our expansion and enable us to provide help and support to our chapters by hosting and sponsoring our workshops.",
   },
 ]
 
@@ -150,24 +148,53 @@ const IndexPage = () => (
     <SEO title="Yemenies Coders" keywords={KEYWORDS} />
     <StyledCover>
       <Wrapper>
-      <StyledHeader className="gradient-color">
-        We are non-profit initiative that facilitates the growth of a diverse tech community by running regular programming workshops.
-      </StyledHeader>
-        <h3 style={{ fontSize: 20, lineHeight: '24px', maxWidth: 600, margin: 'auto', textAlign: "center" }}>
-          Our goal is to enable underrepresented people to learn programming in a safe and collaborative environment and expand their career opportunities. To achieve this we run free regular workshops, regular one-off events and try to create opportunities for our students making technology and coding more accessible.
+        <StyledHeader className="gradient-color">
+          We are non-profit initiative that facilitates the growth of a diverse
+          tech community by running regular programming workshops.
+        </StyledHeader>
+        <h3
+          style={{
+            fontSize: 20,
+            lineHeight: "24px",
+            maxWidth: 600,
+            margin: "auto",
+            textAlign: "center",
+          }}
+        >
+          Our goal is to enable underrepresented people to learn programming in
+          a safe and collaborative environment and expand their career
+          opportunities. To achieve this we run free regular workshops, regular
+          one-off events and try to create opportunities for our students making
+          technology and coding more accessible.
         </h3>
-        <StyledSpan><ChevronDown size={30} color="#999" /></StyledSpan>
+        <StyledSpan>
+          <ChevronDown size={30} color="#999" />
+        </StyledSpan>
       </Wrapper>
     </StyledCover>
     <div>
-      {sections.map((section, index) => <Section key={index} {...section} />)}
+      {sections.map((section, index) => (
+        <Section key={index} {...section} />
+      ))}
     </div>
     <Wrapper id="contatct-us" style={{ paddingTop: 50, paddingBottom: 50 }}>
       <BigH2>Contact us</BigH2>
       <StyledForm>
         <Input placeholder="your name" />
-        <Input placeholder="placeholder" />
-        <Input placeholder="placeholder" />
+        <Input type="email" placeholder="Your Email" />
+        <div style={{ marginBottom: 20, marginTop: 20 }}>
+          <label htmlFor="role">Role</label>
+          <br />
+          <input name="role" type="radio" value="teacher" />
+          Teacher
+          <br />
+          <input name="role" type="radio" value="student" />
+          Student
+          <br />
+          <input name="role" type="radio" value="workshop" />
+          Workshop
+          <br />
+        </div>
         <Input placeholder="placeholder" />
       </StyledForm>
     </Wrapper>
